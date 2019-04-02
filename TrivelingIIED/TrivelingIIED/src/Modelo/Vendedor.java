e494ce615730986271b0a9b25545b683511f45bf
IagoeeeplabSoft <47716243+IagoeeeplabSoft@users.noreply.github.com>
2019-04-01 21:46:49 -0300

    Add files via upload

---
+++ TrivelingIIED/TrivelingIIED/src/Modelo/Vendedor.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Traveling IIED
+ */
+public class Vendedor extends Funcionario{
+     private String login;
+    private String senha;
+    private int codigoVendedor;
+}
